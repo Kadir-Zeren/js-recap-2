93e6bfb22a8919ae38286f3860016b333563732e
kadir-zeren <k4dzeren@gmail.com>
2023-11-19 23:27:41 +0300

    v4

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
-console.log("javascript");
+// console.log("javascript");
 
-var number = 5;
-numberTry = 6;
+// var number = 5;
+// numberTry = 6;
 
-let myVar = 5;
+// let myVar = 5;
 
-const myVar1 = 6;
+// const myVar1 = 6;
 
-const num1 = 10;
-const num2 = 10;
+// const num1 = 10;
+// const num2 = 10;
 
-const toplam = num1 + num2;
+// const toplam = num1 + num2;
 
-console.log(toplam);
+// console.log(toplam);
 
-const msg1 = "Hello";
-const msg2 = "World";
+// const msg1 = "Hello";
+// const msg2 = "World";
 
-const message = msg1 + msg2;
+// const message = msg1 + msg2;
 
-console.log(message);
+// console.log(message);
 
-const firstName = "Koray";
-const lastName = "Koksal";
+// const firstName = "Koray";
+// const lastName = "Koksal";
 
-console.log(firstName, "", lastName);
+// console.log(firstName, "", lastName);
 
-const fullName = firstName + " " + lastName;
-console.log(fullName);
+// const fullName = firstName + " " + lastName;
+// console.log(fullName);
 
-const fullName2 = `${firstName} ${lastName}`;
+// const fullName2 = `${firstName} ${lastName}`;
 
-console.log(fullName2);
+// console.log(fullName2);
 
-const num3 = "6";
-const num4 = "7";
-console.log(num3 + num4);
-console.log(num3 + 7);
+// const num3 = "6";
+// const num4 = "7";
+// console.log(num3 + num4);
+// console.log(num3 + 7);
+
+// console.log(typeof (7 + num3));
+
+const x = 55;
+const y = 44;
+
+console.log(x - y);
+
+const myNum = "1";
+console.log(x - myNum);
+
+const yourNum = "one";
+console.log(x - yourNum);
+
+console.log(x - "a");
+
+let carp1 = 7;
+let carp2 = 9;
+
+console.log(carp1 * carp2);
+
+console.log("a" * "b");
+console.log("a" * 3);
+
+console.log("5" * "9");
+
+console.log(typeof ("5" * "9"));
