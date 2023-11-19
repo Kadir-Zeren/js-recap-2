41261d10f563be3ee9623631820f8b51e8268787
kadir-zeren <k4dzeren@gmail.com>
2023-11-19 22:36:32 +0300

    v2

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("javascript");
+
+var number = 5;
+numberTry = 6;
+
+let myVar = 5;
+
+const myVar1 = 6;
+
+const num1 = 10;
+const num2 = 10;
+
+const toplam = num1 + num2;
+
+console.log(toplam);
